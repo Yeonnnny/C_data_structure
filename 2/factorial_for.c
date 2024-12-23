326c333654b5a810861382e6607af23692b22e98
Yeonnnny <kdoy030@gmail.com>
2024-12-23 15:18:51 +0900

    팩토리얼 반복문으로 구현 - 함수로 분리

--- 2/factorial_for.c
+++ 2/factorial_for.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void)
+int factorial(int n)
 {
+	int i, result = 1;
 
-	int result = 1, n = 3;
-
-	for(int i = 1; i <= n; i++)
+	for(i = 1; i <= n; i++)
 	{
 	    result *= i;
 	    printf("factorial(%d) = %d\n", i, result);
 	}
-	return 0;
+	return result;
+}
 
+int main(void)
+{
+	factorial(3);
+	return 0;
 }
