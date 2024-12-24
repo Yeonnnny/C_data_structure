c4263051f13c14ed2b6a1920b9820ae86cf8a7d9
Yeonnnny <kdoy030@gmail.com>
2024-12-24 18:48:15 +0900

    순환 - 피보나치 시간 비교 & 연습문제에 나온 새로운 함수 추가 후 확인

--- 2/fib.c
+++ 2/fib.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 // 피보나치 수열
 int fib(int n)
@@ -27,12 +28,29 @@ int for_fib(int n)
 	return result;
 }
 
+int new_fib(int n)
+{
+	printf("fib(%d)is called \n", n);
+	if(n==0) return 0;
+	if(n==1) return 1;
+	return (new_fib(n-1)+new_fib(n-2));
+}
 
 int main(void)
 {
-
+	clock_t stop, start;
 	int n = 6;
-	int result = fib(6);
-	printf("순환 fib(%d) = %d\n", n, result);
-	printf("반복 fib(%d) = %d\n", n, for_fib(n));
+	start = clock();
+	printf("순환 fib(%d) = %d ", n, fib(6));
+	stop = clock();
+	double t1 = (double)(stop-start)/CLOCKS_PER_SEC;
+	printf("(%f sec)\n", t1);
+
+	start=clock();
+	printf("반복 fib(%d) = %d ", n, for_fib(n));
+	stop = clock();
+	double t2 = (double)(stop-start)/CLOCKS_PER_SEC;
+	printf("(%f sec)\n", t2);
+
+//	new_fib(6);
 }
